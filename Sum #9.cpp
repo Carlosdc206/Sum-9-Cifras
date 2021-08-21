952e4cad43fd3beca590c17caab43ab111c85916
Carlosdc206 <89317743+Carlosdc206@users.noreply.github.com>
2021-08-21 17:05:28 -0500

    Add files via upload

---
+++ Sum #9.cpp
@@ -0,0 +1,18 @@
+/*Calcular y presentar cuantas cifras tiene un determinado valor numerico introducido por teclado*/
+
+using namespace std;
+
+int main() {
+		int numero = 5;
+		int cifras = 1;
+		
+		while (numero >= 10) {
+			  numero /= 10;
+			  cifras++2;
+		}
+		printf("tiene 2 cifras", cifras);
+		
+		
+		printf ("\n\n\n"); system("PAUSE");
+		return 0;
+}
